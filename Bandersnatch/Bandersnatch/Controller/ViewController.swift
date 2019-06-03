f35656802c591148c16fe47e59e23662e15e5b4b
Daniel Barclay <barclaysd@me.com>
2019-06-03 21:14:13 +0100

    Added logic to determining story path

--- Bandersnatch/Bandersnatch/Controller/ViewController.swift
+++ Bandersnatch/Bandersnatch/Controller/ViewController.swift
@@ -13,13 +13,16 @@ class ViewController: UIViewController {
     let allPages : StoryPageBank = StoryPageBank()
     var currentPage: StoryPages!
     var page: Int = 0
+    var nextPage: Int!
 
+    let imageOptions: Array = ["BlackMirror-C", "BlackMirror-WB"]
     
     // UI Elements linked to the storyboard
-    @IBOutlet weak var topButton: UIButton!         // Has TAG = 1
-    @IBOutlet weak var bottomButton: UIButton!      // Has TAG = 2
+    @IBOutlet weak var topButton: UIButton!
+    @IBOutlet weak var bottomButton: UIButton!
     @IBOutlet weak var storyTextView: UILabel!
-
+    @IBOutlet weak var imageBackground: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
@@ -28,27 +31,46 @@ class ViewController: UIViewController {
     // User presses one of the buttons
     @IBAction func buttonPressed(_ sender: UIButton) {
         
-        page += 1
+        if sender.tag == 1 {
+            switch page {
+            case 0:
+                nextPage = 2
+            case 1:
+                nextPage = 2
+            case 2:
+                nextPage = 5
+            default:
+                print("illegal option")
+            }
+        } else if sender.tag == 2 {
+            switch page {
+            case 0:
+                nextPage = 1
+            case 1:
+                nextPage = 3
+            case 2:
+                nextPage = 4
+            default:
+                print("illegal option")
+            }
+        }
         nextChapter()
         updateUI()
-        // TODO Step 4: Write an IF-Statement to update the views
-        
-        // TODO Step 6: Modify the IF-Statement to complete the story
-        
     }
     
     func updateUI() {
         storyTextView.text = allPages.pageList[page].pageText
         topButton.setTitle(allPages.pageList[page].pageOptions[0], for: .normal)
         bottomButton.setTitle(allPages.pageList[page].pageOptions[1], for: .normal)
+        if page > 2 {
+            imageBackground.image = UIImage.init(named: imageOptions[0])
+            storyTextView.textColor = UIColor.black
+        }
     }
     
     func nextChapter() {
-        if page+1 <= allPages.pageList.count {
-            currentPage = allPages.pageList[page]
-        } else {
-            print("End of story")
-        }
+        page = nextPage
+        currentPage = allPages.pageList[page]
     }
 
 }
