9d5de106087783cd8fae66adf24b051466bf8be0
Daniel Barclay <barclaysd@me.com>
2019-06-08 21:06:11 +0100

    Designed constraints and layout for calculator using Stack Views

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Daniel Barclay on 08/06/2019.
+//  Copyright © 2019 Daniel Barclay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func buttonPressed(_ sender: Any) {
+    }
+    
+}
+
