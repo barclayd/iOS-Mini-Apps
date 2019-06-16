5b5d8497c79971290319fafd28524f7e19461409
Daniel Barclay <barclaysd@me.com>
2019-06-16 20:46:28 +0100

    Fixed issue in making further calculations after the result of one query was 0. Improved display of long decimals

---
+++ Classes and Object/Classes and Object/SelfDrivingCar.swift
@@ -0,0 +1,9 @@
+//
+//  SelfDrivingCar.swift
+//  Classes and Object
+//
+//  Created by Daniel Barclay on 09/06/2019.
+//  Copyright © 2019 Daniel Barclay. All rights reserved.
+//
+
+import Foundation
---
+++ Classes and Object/Classes and Object/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Classes and Object
+//
+//  Created by Daniel Barclay on 09/06/2019.
+//  Copyright © 2019 Daniel Barclay. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
