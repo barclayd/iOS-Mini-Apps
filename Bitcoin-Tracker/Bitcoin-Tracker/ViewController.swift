a3477a12dcc69e28b3bb573348f8b8faf6fdbd1f
Daniel Barclay <barclaysd@me.com>
2019-06-21 22:36:34 +0100

    Fixed bug with Main.storyboard and added Cocoapods

--- Bitcoin-Tracker/Bitcoin-Tracker/ViewController.swift
+++ Bitcoin-Tracker/Bitcoin-Tracker/ViewController.swift
@@ -8,13 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
+    let currencyList = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
+    @IBOutlet weak var bitcoinPriceLabel: UILabel!
+    
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        currencyPicker.delegate = self
+        currencyPicker.dataSource = self
     }
-
-
+    
+    //MARK: Protocol
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return currencyList.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return currencyList[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let currencySelect: String = currencyList[row]
+        
+    }
+    
+    //MARK: Server request
+    func getBitcoinPrice(currency: String) {
+        
+    }
 }
 
