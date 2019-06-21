a411b26a7c9f61889ec3a98381583cccc0ec74ca
Daniel Barclay <barclaysd@me.com>
2019-06-21 21:59:38 +0100

    Initial files for bitcoin tracker

---
+++ Bitcoin Tracker/Bitcoin-Tracker/Bitcoin-Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bitcoin-Tracker
+//
+//  Created by Daniel Barclay on 21/06/2019.
+//  Copyright © 2019 Daniel Barclay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
