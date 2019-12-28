acf788a7f48823f770bfe1897f170821771b73ec
Daniel Barclay <barclaysd@me.com>
2019-12-28 15:46:41 +0000

    Play sound on repeat upon timer completion and show alert

--- Egg Timer/EggTimer/ViewController.swift
+++ Egg Timer/EggTimer/ViewController.swift
@@ -3,13 +3,21 @@
 //  EggTimer
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
     
     var timerLength: Int = 0
     var counter = 0
     
     var eggAlert: UIAlertController?
+    
+    var timerSound: AVAudioPlayer?
     
     var timer = Timer()
     
@@ -43,12 +51,30 @@ class ViewController: UIViewController {
         print("counter: \(counter)")
         if (counter == timerLength) {
             self.showAlert()
+            self.playSound(soundFile: "alarm_sound.mp3")
             timer.invalidate()
         }
         counter += 1
     }
     
+    func playSound(soundFile: String) {
+        
+        let path = Bundle.main.path(forResource: soundFile, ofType:nil)!
+        
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            timerSound = try AVAudioPlayer(contentsOf: url)
+            timerSound?.numberOfLoops = -1
+            timerSound?.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
     func reset() {
+        timerSound?.stop()
         counter = 0
         timerLength = 0
     }
