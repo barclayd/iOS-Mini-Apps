3f953d99ea9db75489664992670d3fdb4b50cea5
Daniel Barclay <barclaysd@me.com>
2019-12-28 23:31:03 +0000

    Logic for getting slider values and updating UI labels

--- BMI Calculator/BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/BMI Calculator/Controllers/ViewController.swift
@@ -6,12 +6,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    @IBOutlet weak var calculateBMI: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        calculateBMI.layer.cornerRadius = 25
     }
 
-
+    @IBAction func heightSlider(_ height: UISlider) {
+        updateLabelFromSlider(label: heightLabel, slider: heightSlider, measurement: "m")
+    }
+    
+    @IBAction func weightSlider(_ sender: UISlider) {
+                updateLabelFromSlider(label: weightLabel, slider: weightSlider, measurement: "KG")
+    }
+    
+    func updateLabelFromSlider(label: UILabel, slider: UISlider, measurement: String) {
+        label.text = "\(String(format: "%.1f", slider.value)) \(measurement)"
+    }
 }
 
