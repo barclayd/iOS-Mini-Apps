5c12252a285dbe0c755b57bb18afa330959507c5
Daniel Barclay <barclaysd@me.com>
2019-12-29 15:29:03 +0000

    Initial commit for Split & Tip

---
+++ Split and Tip/Tipsy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Daniel Barclay on 29/12/2019.
+//  Copyright © 2019 Daniel Barclay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
