1151c83522abe99178e116b1a8d54924a5728a35
Daniel Barclay <barclaysd@me.com>
2019-12-30 15:52:19 +0000

    Initial commit 🚀 for sleep calculator 😴

---
+++ Sleep-Calculator/Sleep-Calculator/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Sleep-Calculator
+//
+//  Created by Daniel Barclay on 30/12/2019.
+//  Copyright © 2019 Daniel Barclay. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var sleepAmount = 8.0
+    
+    var body: some View {
+        Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+            Text("\(sleepAmount, specifier: "%g") hours")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
