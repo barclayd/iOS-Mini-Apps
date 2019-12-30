9de224dbe0cb3a5ceb8037c49a84ee6100088766
Daniel Barclay <barclaysd@me.com>
2019-12-30 11:45:26 +0000

    Improve UI and implement computed property to auto-calculate bill when user input changes

--- Split-and-Tip-Swift-UI/Split-and-Tip-Swift-UI/ContentView.swift
+++ Split-and-Tip-Swift-UI/Split-and-Tip-Swift-UI/ContentView.swift
@@ -11,24 +11,50 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var billAmount = ""
-    @State private var tipPercentage = 10
-    @State private var splitNumber = 1
+    @State private var tipPercentage = 1
+    @State private var splitNumber = 0
     
-    let tipPercentages = [5, 10, 15, 20, 25, 0]
+    let tipPercentages = [0, 5, 10, 15, 20, 25]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(splitNumber + 2)
+        let tipSelection = Double(self.tipPercentages[tipPercentage])
+        let bill = Double(billAmount) ?? 0
+        
+        let billWithTip = bill + ((bill/100) * tipSelection)
+        return billWithTip / peopleCount
+    }
+    
     
     var body: some View {
         NavigationView {
             Form {
                 Section {
                     TextField("Amount", text: $billAmount)
-                        .foregroundColor(.blue)
                         .keyboardType(.decimalPad)
+                    Picker("Ways to split", selection: $splitNumber) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
                 }
+                
+                Section(header: Text("How much tip would you like to leave?")) {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(0..<tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
                 Section {
-                    Text("£\(billAmount)")
+                    Text("Total Bill: £\(billAmount)")
+                    Text("Total Bill + Tip: £\(self.totalPerPerson*Double(self.splitNumber + 2), specifier: "%.2f")")
+                    Text("Bill per person: £\(self.totalPerPerson, specifier: "%.2f")")
                 }
             }
-        .navigationBarTitle("Split & Tip")
+            .navigationBarTitle("Split & Tip")
         }
     }
 }
