b57d7d5e6cacb6453809177f316f3878ab83b1ca
Daniel Barclay <barclaysd@me.com>
2019-12-30 22:28:11 +0000

    Read in text file to generate array of words. Accept user input and append to list

--- Anagrams/Anagrams/ContentView.swift
+++ Anagrams/Anagrams/ContentView.swift
@@ -9,19 +9,54 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        List {
-            Text("Look at me, I'm static!").font(.headline)
-            Section(header: Text("Section 1")) {
-                ForEach(0..<5) {
-                    Text("Hello there, \($0)")
-                }
+    
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
+    let dogs = ["Nodster", "Tedster", "Rooster"]
+    
+    func checkSpellingOfWord(word: String) -> Bool {
+            let checker = UITextChecker()
+            let range = NSRange(location: 0, length: word.utf16.count)
+            let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+            let isValidWord = misspelledRange.location == NSNotFound
+            return isValidWord
+        }
+        
+        func addNewWord() {
+            let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+            guard answer.count > 0 else {
+                return
             }
-            Section(header: Text("Section 2")) {
-                Text("Look at me, I'm static also!")
+            usedWords.insert(answer, at: 0)
+            newWord = ""
+        }
+    
+    var body: some View {
+        
+        if let fileURL = Bundle.main.url(forResource: "words", withExtension: "txt") {
+            if let fileContents = try? String(contentsOf: fileURL) {
+                let words = fileContents.components(separatedBy: "\n")
+                let word = words.randomElement()
+                let trimmedWord = word?.trimmingCharacters(in: .whitespacesAndNewlines)
+                print(trimmedWord!)
             }
         }
-        .listStyle(GroupedListStyle())
+        
+        return NavigationView {
+            VStack {
+                TextField("Enter your word", text: $newWord, onCommit: addNewWord).textFieldStyle(RoundedBorderTextFieldStyle())
+                    .autocapitalization(.none)
+                    .padding()
+                List(usedWords, id: \.self) {
+                    Image(systemName: "\($0.count).circle")
+                    Text($0)
+                        .listStyle(GroupedListStyle())
+                }
+            }
+            .navigationBarTitle(rootWord)
+        }
     }
 }
 
