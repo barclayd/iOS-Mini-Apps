01163d812d7d16620dc40771715ea51f608c3e24
Daniel Barclay <barclaysd@me.com>
2019-12-30 17:51:22 +0000

    Improve UI to support user input

--- Sleep-Calculator/Sleep-Calculator/ContentView.swift
+++ Sleep-Calculator/Sleep-Calculator/ContentView.swift
@@ -11,11 +11,42 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    @State private var wakeUp = Date()
     
     var body: some View {
-        Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
-            Text("\(sleepAmount, specifier: "%g") hours")
+        NavigationView {
+            VStack {
+                Form {
+                    Section {
+                        Text("Desired amount of sleep").frame(maxWidth: .infinity, alignment: .center).font(.headline)
+                        Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+                            Text("\(sleepAmount, specifier: "%g") hours")
+                        }.padding()
+                    }
+                    Section {
+                        Text("When would you like to wake up?").frame(maxWidth: .infinity, alignment: .center).font(.headline)
+                        DatePicker("Please select a time", selection: $wakeUp,
+                                   displayedComponents: .hourAndMinute)
+                    }
+                    
+                    Section {
+                        Text("Daily Coffee Intake").frame(maxWidth: .infinity, alignment: .center).font(.headline)
+                        Stepper(value: $coffeeAmount, in: 1...20, step: 1) {
+                            Text("\(coffeeAmount) \(coffeeAmount > 1 ? "cups" : "cup")")
+                        }.padding()
+                    }
+                }
+            }
+            .navigationBarTitle("Sleep Calculator", displayMode: .inline)
+            .navigationBarItems(trailing: Button(action: calculateBedTime) {
+                Text("Calculate")
+            })
         }
+    }
+    
+    func calculateBedTime() {
+        
     }
 }
 
