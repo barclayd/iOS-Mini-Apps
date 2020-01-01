c1a38c281fa8973de4108b51beacf3e4b314bbce
Daniel Barclay <barclaysd@me.com>
2020-01-01 12:19:07 +0000

    ExpenseItem model, class and implementation

---
+++ Expense-Tracker/Expense-Tracker/Models/ExpenseItem.swift
@@ -0,0 +1,24 @@
+//
+//  ExpenseItem.swift
+//  Expense-Tracker
+//
+//  Created by Daniel Barclay on 01/01/2020.
+//  Copyright © 2020 Daniel Barclay. All rights reserved.
+//
+
+import SwiftUI
+
+enum ExpenseType {
+    case business
+    case leisure
+}
+
+struct ExpenseItem {
+    var name: String
+    var type: ExpenseType
+    var cost: Double
+}
+
+class Expenses: ObservableObject {
+    @Published var items = [ExpenseItem]()
+}
