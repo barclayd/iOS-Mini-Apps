60c65130b898098f5b5a4d842cb954316e102a72
Daniel Barclay <barclaysd@me.com>
2020-01-01 17:42:49 +0000

    Bundle decoder extension and Astronaut model

---
+++ Rocket-Launch/Rocket-Launch/Extensions/Bundle-Decodable.swift
@@ -0,0 +1,25 @@
+//
+//  Bundle-Decodable.swift
+//  Rocket-Launch
+//
+//  Created by Daniel Barclay on 01/01/2020.
+//  Copyright © 2020 Daniel Barclay. All rights reserved.
+//
+
+import Foundation
+
+extension Bundle {
+    func decode(_ file: String) -> [Astronaut] {
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle")
+        }
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) in bundle")
+        }
+        let decoder = JSONDecoder()
+        guard let loaded = try? decoder.decode([Astronaut].self, from: data) else {
+            fatalError("Failed to decode \(file) in bundle")
+        }
+        return loaded
+    }
+}
