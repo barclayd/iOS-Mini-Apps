dff7ebed968372aeb08f6e30bc7f99db3f150b55
Daniel Barclay <barclaysd@me.com>
2020-01-02 13:49:05 +0000

    SpecialEffect and SaturateBlur View

---
+++ Spirograph/Spirograph/Views/SpecialEffectsView.swift
@@ -0,0 +1,48 @@
+//
+//  SpecialEffectsView.swift
+//  Spirograph
+//
+//  Created by Daniel Barclay on 02/01/2020.
+//  Copyright © 2020 Daniel Barclay. All rights reserved.
+//
+
+import SwiftUI
+
+struct SpecialEffectsView: View {
+    @State private var amount: CGFloat = 0.0
+
+    var body: some View {
+        VStack {
+            Image("pawprints")
+                .resizable().scaledToFit().frame(width: 200)
+                .colorMultiply(.orange)
+            ZStack {
+                Circle()
+                    .fill(Color(red: 1, green: 0, blue: 0))
+                    .frame(width: 200 * amount)
+                    .offset(x: -50, y: -80)
+                    .blendMode(.screen)
+                Circle()
+                    .fill(Color(red: 0, green: 1, blue: 0))
+                    .frame(width: 200 * amount)
+                    .offset(x: 50, y: -80)
+                    .blendMode(.screen)
+                Circle()
+                    .fill(Color(red: 0, green: 0, blue: 1))
+                    .frame(width: 200 * amount)
+                    .blendMode(.screen)
+            }
+            .frame(width: 300, height: 300)
+            Slider(value: $amount).padding()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black)
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct SpecialEffectsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SpecialEffectsView()
+    }
+}
