59400a15ebd1d07b7b4ab70685c28571306fb587
Daniel Barclay <barclaysd@me.com>
2020-01-02 22:47:57 +0000

    Encode and send HTTP POST with order information. Decode and display to user to confirm order

--- SweetTreats/SweetTreats/Views/CheckoutView.swift
+++ SweetTreats/SweetTreats/Views/CheckoutView.swift
@@ -11,6 +11,36 @@ import SwiftUI
 struct CheckoutView: View {
     @ObservedObject var order: Order
 
+    @State private var confirmationMessage = ""
+    @State private var showConfirmationMessage = false
+
+    func placeOrder() {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        let url = URL(string: "https://reqres.in/api/icecream")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        request.httpBody = encoded
+
+        URLSession.shared.dataTask(with: request) { data, _, error in
+            guard let data = data else {
+                print("No data in response \(error?.localizedDescription ?? "Unknown error").")
+                return
+            }
+
+            if let decodedOrder = try? JSONDecoder().decode(Order.self, from: data) {
+                self.confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.flavours[decodedOrder.flavour].lowercased()) icecreams are being carefully prepared by our master craftsmen!\n\n They will be with you shortly"
+                self.showConfirmationMessage = true
+            } else {
+                print("Invalid response from server")
+            }
+
+        }.resume()
+    }
+
     var body: some View {
         GeometryReader { geo in
             ScrollView {
@@ -23,10 +53,14 @@ struct CheckoutView: View {
                     Text("Your total order cost: £\(self.order.cost, specifier: "%.2f")")
                         .font(.title)
 
-                    Button("Place Order") {}.padding()
+                    Button("Place Order") {
+                        self.placeOrder()
+                    }.padding()
                 }
             }
-        }.navigationBarTitle("Checkout")
+        }.navigationBarTitle("Checkout").alert(isPresented: $showConfirmationMessage) {
+            Alert(title: Text("Order Complete"), message: Text(confirmationMessage), dismissButton: .default(Text("OK")))
+        }
     }
 }
 
