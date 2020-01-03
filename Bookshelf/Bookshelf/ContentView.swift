2df15542b3ad0d1b8d7fa76383267c1dd6c90fe1
Daniel Barclay <barclaysd@me.com>
2020-01-03 09:11:47 +0000

    Initial commit with Core Data setup

---
+++ Bookshelf/Bookshelf/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  Bookshelf
+//
+//  Created by Daniel Barclay on 03/01/2020.
+//  Copyright © 2020 Daniel Barclay. All rights reserved.
+//
+
+import SwiftUI
+
+struct PushButton: View {
+    let title: String
+    @Binding var isOn: Bool
+
+    var onColours = [Color.red, Color.yellow]
+    var offColours = [Color(white: 0.6), Color(white: 0.4)]
+
+    var body: some View {
+        Button(title) {
+            self.isOn.toggle()
+        }
+        .padding()
+        .background(LinearGradient(gradient: Gradient(colors: isOn ? onColours : offColours), startPoint: .top, endPoint: .bottom))
+        .foregroundColor(.white)
+        .clipShape(Capsule())
+        .shadow(radius: isOn ? 0 : 5)
+    }
+}
+
+struct ContentView: View {
+    @State private var rememberMe = false
+
+    var body: some View {
+        VStack {
+            PushButton(title: "Remember Me", isOn: $rememberMe)
+            Text(rememberMe ? "On" : "Off")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
